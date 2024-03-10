caece230c34675883f72aed25c5cabdcbbf58520
Tanex05 <tanexmj@gmail.com>
2024-03-10 21:35:13 +0800

    Minor Update

--- database/migrations/2024_01_20_121411_create_products_table.php
+++ database/migrations/2024_01_20_121411_create_products_table.php
@@ -18,9 +18,9 @@ return new class extends Migration
             $table->text('thumbnail_image');
 
             $table->integer('category_id');
-            $table->integer('sub_category_id')->nullable;
-            $table->integer('child_category_id')->nullable;
-            $table->integer('brand_id')->nullable;
+            $table->integer('sub_category_id')->nullable();
+            $table->integer('child_category_id')->nullable();
+            $table->integer('brand_id')->nullable();
             $table->integer('qty');
 
             $table->text('short_description');

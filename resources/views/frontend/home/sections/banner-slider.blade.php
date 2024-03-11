001599853f750a63347a4894414085d0fe976e21
Tanex05 <tanexmj@gmail.com>
2024-03-11 19:04:27 +0800

    update

--- resources/views/frontend/home/sections/banner-slider.blade.php
+++ resources/views/frontend/home/sections/banner-slider.blade.php
@@ -1,10 +1,10 @@
+@isset($sliders)
 <section id="wsus__banner">
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
                 <div class="wsus__banner_content">
                     <div class="row banner_slider">
-                        @isset($sliders)
                             @foreach ($sliders as $slider)
                                 <div class="col-xl-12">
                                     <div class="wsus__single_slider" style="background: url({{ $slider->banner }});">
@@ -17,10 +17,10 @@
                                     </div>
                                 </div>
                             @endforeach
-                        @endisset
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+@endisset

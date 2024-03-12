a37c20867d776e7d5e10b46d16b49738c79eb6bc
Tanex05 <tanexmj@gmail.com>
2024-03-12 11:37:20 +0800

    removed foreach in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,22 +23,5 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
-        // Ensure the required directories exist and have write permissions
-        $paths = [
-            'public/flashoutimage',
-            'public/flashsaleimage',
-            'public/uploads',
-            'public/backgrounds',
-        ];
-
-        foreach ($paths as $path) {
-            $directory = public_path($path);
-            if (!File::exists($directory)) {
-                File::makeDirectory($directory, 0755, true);
-            } else {
-                File::chmod($directory, 0755);
-            }
-        }
-
     }
 }
